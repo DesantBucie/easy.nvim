b56b0a2dd5b01fbfcfae4cc25cfa15ab12ebf94d
DesantBucie <mattrusz@gmail.com>
2022-01-10 21:06:30 +0100

    fixed path again

--- lua/path.lua
+++ lua/path.lua
@@ -1,4 +1,4 @@
-pack = os.getenv("HOME")..'/local/share/nvim/site/pack/plugins/'
+pack = os.getenv("HOME")..'/.local/share/nvim/site/pack/plugins/'
 plugins_folder = pack..'start/'
 unloaded_folder = pack..'opt/'
 templates = plugins_folder..'easy.nvim/templates/'
